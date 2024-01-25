f6770adf510628df0b9168639c5c70ace004e475
mssantostuc <mssantostuc@gmail.com>
2024-01-25 05:40:08 -0300

    anotacoes jpa para classes de dominio

--- src/main/java/com/mssantos/helpdesk/domain/Chamado.java
+++ src/main/java/com/mssantos/helpdesk/domain/Chamado.java
@@ -1,22 +1,44 @@
 package com.mssantos.helpdesk.domain;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.Objects;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.mssantos.helpdesk.domain.enums.Prioridade;
 import com.mssantos.helpdesk.domain.enums.Status;
 
-public class Chamado {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 
+@Entity
+public class Chamado implements Serializable {
+
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
-	private LocalDate dataAbertura = LocalDate.now();
+	@JsonFormat(pattern = "dd/MM/yyyy")
+	private LocalDate dataAbertura = LocalDate.now();	
+	@JsonFormat(pattern = "dd/MM/yyyy")
 	private LocalDate dataFechamento;
 	private Prioridade prioridade;
 	private Status status;
 	private String titulo;
 	private String observacoes;
 	
+	@ManyToOne
+	@JoinColumn(name = "tecnico_id")
 	private Tecnico tecnico;
+	
+	@ManyToOne
+	@JoinColumn(name = "cliente_id")
 	private Cliente cliente;
 
 	public Chamado() {
