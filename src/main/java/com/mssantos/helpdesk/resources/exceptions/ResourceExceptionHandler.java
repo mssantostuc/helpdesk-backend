055d0e06aa113a1c9eee58aee29ac588bcd12256
mssantostuc <mssantostuc@gmail.com>
2024-02-21 05:34:52 -0300

    Exceção para objeto não encontrado

---
+++ src/main/java/com/mssantos/helpdesk/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.mssantos.helpdesk.resources.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.mssantos.helpdesk.services.exceptions.ObjectnotFoundException;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+	
+	@ExceptionHandler(ObjectnotFoundException.class)
+	public ResponseEntity<StandardError> objectnotFoundException(ObjectnotFoundException ex, 
+			HttpServletRequest request){
+		
+		StandardError error = new StandardError(System.currentTimeMillis(),HttpStatus.NOT_FOUND.value(),
+				"Object Not Found",ex.getMessage(),request.getRequestURI());
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+	}
+
+}
