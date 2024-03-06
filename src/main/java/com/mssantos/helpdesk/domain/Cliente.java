b66c598d54b7d7b2c7d3b4c7a56841053a9f129b
mssantostuc <mssantostuc@gmail.com>
2024-03-06 08:14:46 -0300

    CRUD para cliente implementado

--- src/main/java/com/mssantos/helpdesk/domain/Cliente.java
+++ src/main/java/com/mssantos/helpdesk/domain/Cliente.java
@@ -2,8 +2,10 @@ package com.mssantos.helpdesk.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.mssantos.helpdesk.domain.dtos.ClienteDTO;
 import com.mssantos.helpdesk.domain.enums.Perfil;
 
 import jakarta.persistence.Entity;
@@ -26,6 +28,17 @@ public class Cliente extends Pessoa {
 	public Cliente(Integer id, String nome, String cpf, String email, String senha) {
 		super(id, nome, cpf, email, senha);
 		addPerfil(Perfil.CLIENTE);
+	}
+	
+	public Cliente(ClienteDTO obj) {
+		super();
+		this.id = obj.getId();
+		this.nome = obj.getNome();
+		this.cpf = obj.getCpf();
+		this.email = obj.getEmail();
+		this.senha = obj.getSenha();
+		this.perfis = obj.getPerfis().stream().map(x -> x.getCodigo()).collect(Collectors.toSet());
+		this.dataCriacao = obj.getDataCriacao();
 	}
 
 	public List<Chamado> getChamados() {
