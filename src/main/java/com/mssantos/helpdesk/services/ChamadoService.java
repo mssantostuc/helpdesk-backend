af56a53951c5c67fb56d9ddeec98d835c3dcd617
mssantostuc <mssantostuc@gmail.com>
2024-03-07 08:36:00 -0300

    findById para Chamado

---
+++ src/main/java/com/mssantos/helpdesk/services/ChamadoService.java
@@ -0,0 +1,23 @@
+package com.mssantos.helpdesk.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mssantos.helpdesk.domain.Chamado;
+import com.mssantos.helpdesk.repositories.ChamadoRepository;
+import com.mssantos.helpdesk.services.exceptions.ObjectnotFoundException;
+
+@Service
+public class ChamadoService {
+	
+	@Autowired
+	private ChamadoRepository repository;
+	
+	public Chamado findById(Integer id) {
+		Optional<Chamado> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectnotFoundException("Objeto não encontrado! Id: "+ id));
+	}
+
+}
