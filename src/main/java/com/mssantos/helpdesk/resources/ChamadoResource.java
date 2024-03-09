f924a55c53ac9e72fbfc88d9a1e1a63b36f864eb
mssantostuc <mssantostuc@gmail.com>
2024-03-09 10:42:58 -0300

    create para Chamado

--- src/main/java/com/mssantos/helpdesk/resources/ChamadoResource.java
+++ src/main/java/com/mssantos/helpdesk/resources/ChamadoResource.java
@@ -1,5 +1,6 @@
 package com.mssantos.helpdesk.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -7,12 +8,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.mssantos.helpdesk.domain.Chamado;
 import com.mssantos.helpdesk.domain.dtos.ChamadoDTO;
 import com.mssantos.helpdesk.services.ChamadoService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping(value = "/chamados")
@@ -32,6 +38,14 @@ public class ChamadoResource {
 		List<Chamado> list = service.findAll();
 		List<ChamadoDTO> listDTO = list.stream().map(obj -> new ChamadoDTO(obj)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDTO);
+	}
+	
+	@PostMapping
+	public ResponseEntity<ChamadoDTO> create(@Valid @RequestBody ChamadoDTO objDTO){
+		Chamado obj = service.create(objDTO);
+		URI uri = ServletUriComponentsBuilder.
+				fromCurrentRequestUri().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 
 }
--- src/main/java/com/mssantos/helpdesk/services/ChamadoService.java
+++ src/main/java/com/mssantos/helpdesk/services/ChamadoService.java
@@ -7,14 +7,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.mssantos.helpdesk.domain.Chamado;
+import com.mssantos.helpdesk.domain.Cliente;
+import com.mssantos.helpdesk.domain.Tecnico;
+import com.mssantos.helpdesk.domain.dtos.ChamadoDTO;
+import com.mssantos.helpdesk.domain.enums.Prioridade;
+import com.mssantos.helpdesk.domain.enums.Status;
 import com.mssantos.helpdesk.repositories.ChamadoRepository;
 import com.mssantos.helpdesk.services.exceptions.ObjectnotFoundException;
+
+import jakarta.validation.Valid;
 
 @Service
 public class ChamadoService {
 	
 	@Autowired
 	private ChamadoRepository repository;
+	@Autowired
+	private TecnicoService tecnicoService;
+	@Autowired
+	private ClienteService clienteService;
 	
 	public Chamado findById(Integer id) {
 		Optional<Chamado> obj = repository.findById(id);
@@ -23,6 +34,27 @@ public class ChamadoService {
 
 	public List<Chamado> findAll() {
 		return repository.findAll();
+	}
+
+	public Chamado create(@Valid ChamadoDTO objDTO) {
+		return repository.save(newChamado(objDTO));
+	}
+	
+	private Chamado newChamado(ChamadoDTO obj) {
+		Tecnico tecnico = tecnicoService.findById(obj.getTecnico());
+		Cliente cliente = clienteService.findById(obj.getCliente());
+		
+		Chamado chamado = new Chamado();
+		if(obj.getId() != null) {
+			chamado.setId(obj.getId());
+		}
+		chamado.setTecnico(tecnico);
+		chamado.setCliente(cliente);
+		chamado.setPrioridade(Prioridade.toEnum(obj.getPrioridade()));
+		chamado.setStatus(Status.toEnum(obj.getStatus()));
+		chamado.setTitulo(obj.getTitulo());
+		chamado.setObservacoes(obj.getObservacoes());
+		return chamado;
 	}
 
 }
