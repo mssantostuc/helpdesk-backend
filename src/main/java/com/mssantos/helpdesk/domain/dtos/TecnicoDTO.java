db4cb15739e5ff1ff580cd76517baf23a532c64a
mssantostuc <mssantostuc@gmail.com>
2024-03-28 09:37:18 -0300

    correcao validacao cpf

--- src/main/java/com/mssantos/helpdesk/domain/dtos/TecnicoDTO.java
+++ src/main/java/com/mssantos/helpdesk/domain/dtos/TecnicoDTO.java
@@ -7,6 +7,7 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 import javax.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.br.CPF;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.mssantos.helpdesk.domain.Tecnico;
@@ -20,6 +21,7 @@ public class TecnicoDTO implements Serializable{
 	@NotNull(message = "O campo NOME é requerido")
 	protected String nome;
 	@NotNull(message = "O campo CPF é requerido")
+	@CPF
 	protected String cpf;
 	@NotNull(message = "O campo EMAIL é requerido")
 	protected String email;
