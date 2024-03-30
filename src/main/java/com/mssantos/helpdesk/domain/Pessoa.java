1bbdbbd852798e37f471afe53452f9b927ee3173
mssantostuc <mssantostuc@gmail.com>
2024-03-30 11:49:46 -0300

    correção de bug

--- src/main/java/com/mssantos/helpdesk/domain/Pessoa.java
+++ src/main/java/com/mssantos/helpdesk/domain/Pessoa.java
@@ -33,7 +33,7 @@ public abstract class Pessoa implements Serializable{
 	protected Integer id;
 	protected String nome;
 	
-	@CPF(message="cpf inválido")
+	@CPF
 	@Column(unique = true)
 	protected String cpf;
 	
